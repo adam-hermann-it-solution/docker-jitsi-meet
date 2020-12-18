947852f41fe06c0d970e761e24c89d3f3d524480
Adam Hermann <ich@adamhermann>
2020-12-18 13:31:51 +0100

    dddd

--- zz_clean.sh
+++ zz_clean.sh
@@ -9,7 +9,7 @@ docker rm docker-jitsi-meet_jvb_1
 docker rm docker-jitsi-meet_web_1
 docker rm docker-jitsi-meet_prosody_1
 
-sudo rm -r ~/jitsi-meet-cfg
+rm -r ~/jitsi-meet-cfg
 
 mkdir -p ~/jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
 
